5334c738a6ec5e5293d9cd2efc80230303a322c3
goeth <goeth@fim.uni-passau.de>
2020-04-23 03:07:15 +0200

    added premake build file

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+-- premake5.lua
+workspace "neclib"
+	configurations { "Debug", "Release"}
+		toolset "v141_xp"
+		nativewchar "On"
+		system ("Windows")
+		characterset ("MBCS")
+	
+project "neclib"
+	kind "SharedLib"
+	includedirs { "Inc", "minhook-master/include", "boost-1.67.0" }
+	language "C++"
+	targetdir "build/bin/%{cfg.buildcfg}"
+	
+	files { "**.h", "**.hpp", "**.c", "**.cpp", "**.def"}
+	excludes { "Levitation/**", "MinHook/**", "boost-1.67.0/**" }
+	
+	filter "configurations:Debug"
+		defines {"DEBUG", "WIN32", "_DEBUG", "_WINDOWS", "_USRDLL", "DYNITEMINST_EXPORTS"}
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines {"WIN32", "NDEBUG", "_WINDOWS", "_USRDLL", "DYNITEMINST_EXPORTS"}
+		optimize "On"
+		
+	filter "system:Windows"
+      system "windows"	
+		
+		
