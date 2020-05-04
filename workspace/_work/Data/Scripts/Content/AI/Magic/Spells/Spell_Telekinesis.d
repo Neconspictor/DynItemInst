8b4f427957ffdb740285d55b9b87817ed755da05
goeth <goeth@fim.uni-passau.de>
2020-05-04 18:21:02 +0200

    telekinesis spell: changed encoding to ANSI again; replaced tabs by spaces;

--- workspace/_work/Data/Scripts/Content/AI/Magic/Spells/Spell_Telekinesis.d
+++ workspace/_work/Data/Scripts/Content/AI/Magic/Spells/Spell_Telekinesis.d
@@ -2,9 +2,9 @@
 // SPL_Telekinesis
 // ***************
 
-const int SPL_Cost_Telekinesis		= 10;
-const int STEP_Telekinesis			= 1;
-const int SPL_Damage_Telekinesis 		= 0;
+const int SPL_Cost_Telekinesis      = 10;
+const int STEP_Telekinesis          = 1;
+const int SPL_Damage_Telekinesis        = 0;
 const int TELEKINESIS_UPMOVEMENT = 50; // (in cm) the focus item is moved upwards when starting the spell.
 const int TELEKINESIS_SPEED = 150; // (in cm/s) the traveling speed of the focus item.
 const int TELEKINESIS_MOVEMENT_DELAY = 2000; // (in ms) time to wait before the focus item travels
@@ -14,27 +14,27 @@ const string PRINT_TO_FAR_AWAY = "Mmh, das Objekt kann ich nicht sehen...";
 
 INSTANCE Spell_Telekinesis (C_Spell_Proto)
 {
-	time_per_mana			= 0; // Zeit pro investierten Manapunkt (ms)
-	spelltype 				= SPELL_NEUTRAL;
-	damage_per_level		= SPL_Damage_Telekinesis;		
-	damageType				= DAM_MAGIC;				
-	canTurnDuringInvest     = FALSE;
-	
-	canChangeTargetDuringInvest	=	FALSE;
-	isMultiEffect				=	FALSE;
-	targetCollectAlgo			=	TARGET_COLLECT_FOCUS_FALLBACK_NONE; //TARGET_COLLECT_FOCUS_FALLBACK_NONE;//TARGET_COLLECT_FOCUS_FALLBACK_NONE;
-	targetCollectType			=	TARGET_TYPE_ITEMS;
-	targetCollectRange			=	10000;		
-	targetCollectAzi			=	85;
-	targetCollectElev			=	85;
+    time_per_mana           = 0; // Zeit pro investierten Manapunkt (ms)
+    spelltype               = SPELL_NEUTRAL;
+    damage_per_level        = SPL_Damage_Telekinesis;       
+    damageType              = DAM_MAGIC;                
+    canTurnDuringInvest     = FALSE;
+    
+    canChangeTargetDuringInvest =   FALSE;
+    isMultiEffect               =   FALSE;
+    targetCollectAlgo           =   TARGET_COLLECT_FOCUS_FALLBACK_NONE; //TARGET_COLLECT_FOCUS_FALLBACK_NONE;//TARGET_COLLECT_FOCUS_FALLBACK_NONE;
+    targetCollectType           =   TARGET_TYPE_ITEMS;
+    targetCollectRange          =   10000;      
+    targetCollectAzi            =   85;
+    targetCollectElev           =   85;
 };
 
 /**
  * Holds data needed for the telekinese interpolation.
  */
 class Spell_Telekinesis_Data {
-	var int pTarget; // the target item
-	var int pInterpolator; // handle to the telekinese Interpolator
+    var int pTarget; // the target item
+    var int pInterpolator; // handle to the telekinese Interpolator
 };
 
 instance Spell_Telekinesis_Data@(Spell_Telekinesis_Data);
@@ -58,22 +58,22 @@ var Spell_Telekinesis_Data _TelekinesisData;
   * @param speed : A delay before the interplation starts (in milliseconds)
   */
 func int TELEKINESIS_CreateInterpolator(var int pVobPosition, var int pNpcPosition, var int upMoveAmount, var int speed, var int delay) {
-	if (!(NEC_Init_Modules & NEC_TELEKINESIS)) {
+    if (!(NEC_Init_Modules & NEC_TELEKINESIS)) {
         MEM_SendToSpy(zERR_TYPE_WARN, "neclib: TELEKINESIS_CreateInterpolator: Telekinesis module isn't initialized!");
         return 0;
     };
 
     var int ret;
-	var int adr;
-	adr = GetProcAddress (LoadLibrary (NEC_relativeLibraryPath), "TELEKINESIS_CreateInterpolator");
-	CALL_IntParam(delay);
-	CALL_IntParam(speed);
-	CALL_IntParam(upMoveAmount);
-	CALL_IntParam(pNpcPosition);
-	CALL_IntParam(pVobPosition);
-	CALL_PutRetValTo(_@(ret));
-	CALL__cdecl(adr);
-	
+    var int adr;
+    adr = GetProcAddress (LoadLibrary (NEC_relativeLibraryPath), "TELEKINESIS_CreateInterpolator");
+    CALL_IntParam(delay);
+    CALL_IntParam(speed);
+    CALL_IntParam(upMoveAmount);
+    CALL_IntParam(pNpcPosition);
+    CALL_IntParam(pVobPosition);
+    CALL_PutRetValTo(_@(ret));
+    CALL__cdecl(adr);
+    
     return +ret;
 };
 
@@ -84,16 +84,16 @@ func int TELEKINESIS_CreateInterpolator(var int pVobPosition, var int pNpcPositi
  */
 func void TELEKINESIS_GetInterpolatedVec(var int pTelekinesisInterpolator, var int pDestVec3) {
     if (!(NEC_Init_Modules & NEC_TELEKINESIS)) {
-		MEM_SendToSpy(zERR_TYPE_WARN, "neclib: TELEKINESIS_GetInterpolatedVec: Telekinesis module isn't initialized!");
+        MEM_SendToSpy(zERR_TYPE_WARN, "neclib: TELEKINESIS_GetInterpolatedVec: Telekinesis module isn't initialized!");
         return;
     };
 
     var int ret;
-	var int adr;
-	adr = GetProcAddress (LoadLibrary (NEC_relativeLibraryPath), "TELEKINESIS_GetInterpolatedVec");
-	CALL_IntParam(pDestVec3);
-	CALL_IntParam(pTelekinesisInterpolator);
-	CALL__cdecl(adr);
+    var int adr;
+    adr = GetProcAddress (LoadLibrary (NEC_relativeLibraryPath), "TELEKINESIS_GetInterpolatedVec");
+    CALL_IntParam(pDestVec3);
+    CALL_IntParam(pTelekinesisInterpolator);
+    CALL__cdecl(adr);
 };
 
 /**
@@ -102,15 +102,15 @@ func void TELEKINESIS_GetInterpolatedVec(var int pTelekinesisInterpolator, var i
  */
 func void TELEKINESIS_DeleteInterpolator(var int pTelekinesisInterpolator) {
     if (!(NEC_Init_Modules & NEC_TELEKINESIS)) {
-		MEM_SendToSpy(zERR_TYPE_WARN, "neclib: TELEKINESIS_DeleteInterpolator: Telekinesis module isn't initialized!");
+        MEM_SendToSpy(zERR_TYPE_WARN, "neclib: TELEKINESIS_DeleteInterpolator: Telekinesis module isn't initialized!");
         return;
     };
 
     var int ret;
-	var int adr;
-	adr = GetProcAddress (LoadLibrary (NEC_relativeLibraryPath), "TELEKINESIS_DeleteInterpolator");
-	CALL_IntParam(pTelekinesisInterpolator);
-	CALL__cdecl(adr);
+    var int adr;
+    adr = GetProcAddress (LoadLibrary (NEC_relativeLibraryPath), "TELEKINESIS_DeleteInterpolator");
+    CALL_IntParam(pTelekinesisInterpolator);
+    CALL__cdecl(adr);
 };
 
 /**
@@ -120,15 +120,15 @@ func void TELEKINESIS_DeleteInterpolator(var int pTelekinesisInterpolator) {
  */
 func void TELEKINESIS_Interpolate(var int pTelekinesisInterpolator, var int zVec3Ptr) {
     if (!(NEC_Init_Modules & NEC_TELEKINESIS)) {
-		MEM_SendToSpy(zERR_TYPE_WARN, "neclib: TELEKINESIS_Interpolate: Telekinesis module isn't initialized!");
+        MEM_SendToSpy(zERR_TYPE_WARN, "neclib: TELEKINESIS_Interpolate: Telekinesis module isn't initialized!");
         return;
     };
 
-	var int adr;
-	adr = GetProcAddress (LoadLibrary (NEC_relativeLibraryPath), "TELEKINESIS_Interpolate");
-	CALL_IntParam(zVec3Ptr);
-	CALL_IntParam(pTelekinesisInterpolator);
-	CALL__cdecl(adr);
+    var int adr;
+    adr = GetProcAddress (LoadLibrary (NEC_relativeLibraryPath), "TELEKINESIS_Interpolate");
+    CALL_IntParam(zVec3Ptr);
+    CALL_IntParam(pTelekinesisInterpolator);
+    CALL__cdecl(adr);
 };
 
 /**
@@ -138,19 +138,19 @@ func void TELEKINESIS_Interpolate(var int pTelekinesisInterpolator, var int zVec
  * @return : TRUE if the vob is seeable.
  */
 func int TELEKINESIS_IsVobSeeable(var int pNpc, var int pVob) {
-	if (!(NEC_Init_Modules & NEC_TELEKINESIS)) {
-		MEM_SendToSpy(zERR_TYPE_WARN, "neclib: TELEKINESIS_IsVobSeeable: Telekinesis module isn't initialized!");
+    if (!(NEC_Init_Modules & NEC_TELEKINESIS)) {
+        MEM_SendToSpy(zERR_TYPE_WARN, "neclib: TELEKINESIS_IsVobSeeable: Telekinesis module isn't initialized!");
         return 0;
     };
 
     var int ret;
-	var int adr;
-	adr = GetProcAddress (LoadLibrary (NEC_relativeLibraryPath), "TELEKINESIS_IsVobSeeable");
-	CALL_IntParam(pVob);
-	CALL_IntParam(pNpc);
-	CALL_PutRetValTo(_@(ret));
-	CALL__cdecl(adr);
-	
+    var int adr;
+    adr = GetProcAddress (LoadLibrary (NEC_relativeLibraryPath), "TELEKINESIS_IsVobSeeable");
+    CALL_IntParam(pVob);
+    CALL_IntParam(pNpc);
+    CALL_PutRetValTo(_@(ret));
+    CALL__cdecl(adr);
+    
     return +ret;
 };
 
@@ -160,13 +160,13 @@ func int TELEKINESIS_IsVobSeeable(var int pNpc, var int pVob) {
  */
 func void TELEKINESIS_ClearInterpolators() {
     if (!(NEC_Init_Modules & NEC_TELEKINESIS)) {
-		MEM_SendToSpy(zERR_TYPE_WARN, "neclib: TELEKINESIS_ClearInterpolators: Telekinesis module isn't initialized!");
+        MEM_SendToSpy(zERR_TYPE_WARN, "neclib: TELEKINESIS_ClearInterpolators: Telekinesis module isn't initialized!");
         return;
     };
 
-	var int adr;
-	adr = GetProcAddress (LoadLibrary (NEC_relativeLibraryPath), "TELEKINESIS_ClearInterpolators");
-	CALL__cdecl(adr);
+    var int adr;
+    adr = GetProcAddress (LoadLibrary (NEC_relativeLibraryPath), "TELEKINESIS_ClearInterpolators");
+    CALL__cdecl(adr);
 };
 
 
@@ -187,59 +187,59 @@ func void _Spell_Telekinesis_MoveTarget() {
     var oCItem oCTarget; oCTarget = _^(_TelekinesisData.pTarget);
 
     // Continue movement
-	var int interpolatedPosition[3]; //same memory layout as zVEC3
-	
+    var int interpolatedPosition[3]; //same memory layout as zVEC3
+    
     TELEKINESIS_Interpolate(_TelekinesisData.pInterpolator, _@(interpolatedPosition));
-	
-	// Update position of item
-	zCVobSetPositionWorld(_TelekinesisData.pTarget, _@(interpolatedPosition));
+    
+    // Update position of item
+    zCVobSetPositionWorld(_TelekinesisData.pTarget, _@(interpolatedPosition));
 };
 
 func int Spell_Logic_Telekinesis (var int manaInvested)
 {
-	
-	if (manaInvested == 0 && self.attribute[ATR_MANA]<SPL_Cost_Telekinesis) 
-	{
-		return SPL_DONTINVEST;	
-	};
-	
-	if (!Hlp_IsValidNpc(self)) {return SPL_DONTINVEST;};
-	
-	
-	var oCNpc oCSelf; oCSelf = _^(_@(self));
-	
-	if (oCSelf.focus_vob == 0) {return SPL_DONTINVEST;};
-	
-	var oCItem oCFocus; oCFocus = _^(oCSelf.focus_vob);
-	
-	if (!Hlp_IsValidItem(oCFocus)) {return SPL_DONTINVEST;};
-	
-	var int canSee; canSee = TELEKINESIS_IsVobSeeable(_@(self), oCSelf.focus_vob);
-	//MEM_Warn(ConcatStrings("canSee = ", IntToString(canSee)));
-	
-	if (!canSee && (manaInvested == 0)) {
-		Print(PRINT_TO_FAR_AWAY);
-		return SPL_DONTINVEST;
-	};
-	
-	if (manaInvested == 0) {
-	
-		// we substract one mana point less for suppressing auto cast causing spell to stop prematurely 
-		// Spell_Cast_Telekinesis substracts that missing point.
-		self.attribute[ATR_MANA] = (self.attribute[ATR_MANA] - SPL_Cost_Telekinesis + 1);
+    
+    if (manaInvested == 0 && self.attribute[ATR_MANA]<SPL_Cost_Telekinesis) 
+    {
+        return SPL_DONTINVEST;  
+    };
+    
+    if (!Hlp_IsValidNpc(self)) {return SPL_DONTINVEST;};
+    
+    
+    var oCNpc oCSelf; oCSelf = _^(_@(self));
+    
+    if (oCSelf.focus_vob == 0) {return SPL_DONTINVEST;};
+    
+    var oCItem oCFocus; oCFocus = _^(oCSelf.focus_vob);
+    
+    if (!Hlp_IsValidItem(oCFocus)) {return SPL_DONTINVEST;};
+    
+    var int canSee; canSee = TELEKINESIS_IsVobSeeable(_@(self), oCSelf.focus_vob);
+    //MEM_Warn(ConcatStrings("canSee = ", IntToString(canSee)));
+    
+    if (!canSee && (manaInvested == 0)) {
+        Print(PRINT_TO_FAR_AWAY);
+        return SPL_DONTINVEST;
+    };
+    
+    if (manaInvested == 0) {
+    
+        // we substract one mana point less for suppressing auto cast causing spell to stop prematurely 
+        // Spell_Cast_Telekinesis substracts that missing point.
+        self.attribute[ATR_MANA] = (self.attribute[ATR_MANA] - SPL_Cost_Telekinesis + 1);
 
-		if (self.attribute[ATR_MANA]<0) 
-		{
-			self.attribute[ATR_MANA]=0;
-		};
-				
+        if (self.attribute[ATR_MANA]<0) 
+        {
+            self.attribute[ATR_MANA]=0;
+        };
+                
 
         _TelekinesisData.pTarget = oCSelf.focus_vob;
-		
-		
-		var oCItem oCTarget; oCTarget = _^(_TelekinesisData.pTarget);
-		
-		var int vobPosition[3];
+        
+        
+        var oCItem oCTarget; oCTarget = _^(_TelekinesisData.pTarget);
+        
+        var int vobPosition[3];
         vobPosition[0] = oCTarget._zCVob_trafoObjToWorld[3];
         vobPosition[1] = oCTarget._zCVob_trafoObjToWorld[7];
         vobPosition[2] = oCTarget._zCVob_trafoObjToWorld[11];
@@ -249,59 +249,59 @@ func int Spell_Logic_Telekinesis (var int manaInvested)
         npcPosition[1] = oCSelf._zCVob_trafoObjToWorld[7];
         npcPosition[2] = oCSelf._zCVob_trafoObjToWorld[11];
         
-		// Delete the old interpolator
-		// Note: interpolators are handles. Thus, it is ok if the interpolator isn't valid, 
-		// since neclib checks the handle
-		TELEKINESIS_DeleteInterpolator(_TelekinesisData.pInterpolator);
+        // Delete the old interpolator
+        // Note: interpolators are handles. Thus, it is ok if the interpolator isn't valid, 
+        // since neclib checks the handle
+        TELEKINESIS_DeleteInterpolator(_TelekinesisData.pInterpolator);
         _TelekinesisData.pInterpolator = TELEKINESIS_createInterpolator(_@(vobPosition[0]), 
                                                             _@(npcPosition[0]),
                                                             TELEKINESIS_UPMOVEMENT,
                                                             TELEKINESIS_SPEED,
-															TELEKINESIS_MOVEMENT_DELAY);
-		
-		// ensure that npcs react if hero tries to steal
-		if (Npc_IsPlayer(self)) {
-			var int itemBak; itemBak = _@(item); 
+                                                            TELEKINESIS_MOVEMENT_DELAY);
+        
+        // ensure that npcs react if hero tries to steal
+        if (Npc_IsPlayer(self)) {
+            var int itemBak; itemBak = _@(item); 
 
-			item = _^(_TelekinesisData.pTarget);			
-			Npc_SendPassivePerc(self, PERC_ASSESSTHEFT, self, self);
-			MEM_AssignInstSuppressNullWarning = TRUE;
-			item = _^(itemBak);
-			MEM_AssignInstSuppressNullWarning = FALSE;
-		};
-		
-	} else {
-		_Spell_Telekinesis_MoveTarget();
-	};
-	
-	return SPL_NEXTLEVEL;//SPL_NEXTLEVEL;
+            item = _^(_TelekinesisData.pTarget);            
+            Npc_SendPassivePerc(self, PERC_ASSESSTHEFT, self, self);
+            MEM_AssignInstSuppressNullWarning = TRUE;
+            item = _^(itemBak);
+            MEM_AssignInstSuppressNullWarning = FALSE;
+        };
+        
+    } else {
+        _Spell_Telekinesis_MoveTarget();
+    };
+    
+    return SPL_NEXTLEVEL;//SPL_NEXTLEVEL;
 };
 
 func void Spell_Cast_Telekinesis(var int spellLevel)
-{		
-	MEM_Info("neclib: Spell_Cast_Telekinesis: called.");
+{       
+    MEM_Info("neclib: Spell_Cast_Telekinesis: called.");
 
-	// substract the last missing mana point.
-	self.attribute[ATR_MANA] = (self.attribute[ATR_MANA] - 1);
+    // substract the last missing mana point.
+    self.attribute[ATR_MANA] = (self.attribute[ATR_MANA] - 1);
 
-	if (self.attribute[ATR_MANA]<0) 
-	{
-		self.attribute[ATR_MANA]=0;
-	};
-	
-	// Only do something if the player uses the spell
-	if (!Npc_IsPlayer(self)) {
-		return;
-	};
-	
-	// delete telekinesis data	
-	TELEKINESIS_DeleteInterpolator(_TelekinesisData.pInterpolator);
-	
-	var oCItem target; target = _^(_TelekinesisData.pTarget);
-	
-	if (!Hlp_IsValidItem(target)) {return;};
-	
-	zCVobSetSleeping(_TelekinesisData.pTarget, FALSE);
+    if (self.attribute[ATR_MANA]<0) 
+    {
+        self.attribute[ATR_MANA]=0;
+    };
+    
+    // Only do something if the player uses the spell
+    if (!Npc_IsPlayer(self)) {
+        return;
+    };
+    
+    // delete telekinesis data  
+    TELEKINESIS_DeleteInterpolator(_TelekinesisData.pInterpolator);
+    
+    var oCItem target; target = _^(_TelekinesisData.pTarget);
+    
+    if (!Hlp_IsValidItem(target)) {return;};
+    
+    zCVobSetSleeping(_TelekinesisData.pTarget, FALSE);
     zCVobSetPhysicsEnabled(_TelekinesisData.pTarget, TRUE);    
 };
 
@@ -374,13 +374,13 @@ func void Spell_Telekinesis_Prio() {
  */ 
 func void TELEKINESIS_Init() 
 {
-	if (!(NEC_Init_Modules & NEC_TELEKINESIS)) {
+    if (!(NEC_Init_Modules & NEC_TELEKINESIS)) {
         MEM_SendToSpy(zERR_TYPE_FAULT, "neclib: LEVITATION_Init: Telekinesis module isn't specified to be initialized!");
         return;
     };
 
 
-	// Setup hooks for focus
+    // Setup hooks for focus
     const int oCSpell__Setup_G2            = 4737328; //0x484930
     const int oCMag_Book__SetFrontSpell_G2 = 4688320; //004789C0
     HookEngineF(oCSpell__Setup_G2, 7, Spell_Telekinesis_Prio);
@@ -393,15 +393,18 @@ func void TELEKINESIS_Init()
         MEM_Info("neclib: Spell_Telekinesis: Reassigning Focus_Magic instance");
         Focus_Magic = _^(fMagicPtr);
     };
-	
-	// just for safety. Shouldn't be necessary
-	TELEKINESIS_ClearInterpolators();
-	
-	// _TelekinesisData is a global variable we only need to initialize once
-	if (!_@(_TelekinesisData)) {
-		_TelekinesisData = _^(create(Spell_Telekinesis_Data@));
-		MEM_Info("neclib: TELEKINESIS_Init: assigned _TelekinesisData to a new Spell_Telekinesis_Data@ instance");
-	};
-	
-	MEM_Info("neclib: TELEKINESIS_Init: done.");
+    
+    // just for safety. Shouldn't be necessary
+    TELEKINESIS_ClearInterpolators();
+    
+    
+    
+    
+    // _TelekinesisData is a global variable we only need to initialize once
+    if (!_@(_TelekinesisData)) {
+        _TelekinesisData = _^(create(Spell_Telekinesis_Data@));
+        MEM_Info("neclib: TELEKINESIS_Init: assigned _TelekinesisData to a new Spell_Telekinesis_Data@ instance object");
+    };
+    
+    MEM_Info("neclib: TELEKINESIS_Init: done.");
 };
